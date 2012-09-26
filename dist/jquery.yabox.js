671d0004cca600aa2434feeda412a5e40574b3b4
Juho Vepsäläinen <bebraw@gmail.com>
2012-09-26 15:07:44 +0300

    Bind content dynamically on show
    Previously it would just stick to the initial content given at it. Not good for
    certain purposes.

--- dist/jquery.yabox.js
+++ dist/jquery.yabox.js
@@ -2,34 +2,30 @@
 * http://bebraw.github.com/yabox.js/
 * Copyright (c) 2012 Juho Vepsalainen; Licensed MIT */
 
-/* MIT (c) Juho Vepsalainen */
 (function ($) {
     function yabox($elem, opts) {
         var $overlay = $(opts.overlayId).length? $(opts.overlayId): overlay();
         var $full = full();
-        $overlay.bind('click', hide($('.' + opts.fullClass)))
+        $overlay.bind('click', hide($('.' + opts.fullClass)));
+
+        if(opts.$content) opts.$content.hide();
 
         function overlay() {
-            return $('<div>')
+            return $('<div/>')
                 .attr('id', 'overlay')
                 .appendTo($('body'))
                 .hide();
         }
 
         function full() {
-            var $e = opts.$content? opts.$content: $elem.clone();
-
-            $e = $e.hide()
+            var $e = $('<div/>').hide()
                 .css({
                     'z-index': $overlay.css('z-index') + 1
                 })
-                .removeClass()
                 .addClass(opts.fullClass)
                 .appendTo($('body'));
 
-            if(opts.hideOnClick) {
-                $e.bind('click', hide($e));
-            }
+            if(opts.hideOnClick) $e.bind('click', hide($e));
 
             return $e;
         }
@@ -37,14 +33,28 @@
         $elem.bind('click', show);
         function show(e) {
             e.preventDefault();
+
+            var $content;
+            if(opts.$content) {
+                opts.$content.show();
+                $content = opts.$content;
+            }
+            else $content = $elem.clone();
+
+            $content.removeClass();
+
+            $full.html($content);
+
             $full.center();
             opts.showCb($full, $overlay);
         }
 
         function hide($f) {
             return function() {
+                if(opts.$content) opts.$content.hide();
+
                 opts.hideCb($f, $overlay);
-            }
+            };
         }
     }
 
@@ -52,7 +62,7 @@
         this.css({
             'position': 'fixed',
             'left': '50%',
-            'top': '50%',
+            'top': '50%'
         });
         this.css({
             'margin-left': -this.width() / 2 + 'px',
@@ -60,7 +70,7 @@
         });
 
         return this;
-    }
+    };
 
     $.fn.yabox = function(options) {
         return this.each(function () {
