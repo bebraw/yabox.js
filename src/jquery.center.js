799c1cf0e5359d303d8d98747806eb3f129dae58
Juho Vepsäläinen <bebraw@gmail.com>
2012-10-18 21:00:27 +0300

    Add missing center plugin

---
+++ src/jquery.center.js
@@ -0,0 +1,26 @@
+(function ($) {
+    // http://stackoverflow.com/a/210733/228885
+    jQuery.fn.center = function ($parent) {
+        $parent = $parent || $(window);
+
+        var $e = this;
+
+
+        this.css("position","absolute");
+        this.css("top", Math.max(0, (($parent.height() - this.outerHeight()) / 2) +
+            $(window).scrollTop()) + "px");
+        this.css("left", Math.max(0, (($parent.width() - this.outerWidth()) / 2) +
+            $(window).scrollLeft()) + "px");
+
+        if(!$e.data('centered')) {
+            $(window).on('resize', center).on('scroll', center);
+        }
+
+        function center() {
+            $e.center($parent);
+            $e.data('centered', true);
+        }
+
+        return this;
+    };
+})(jQuery);
